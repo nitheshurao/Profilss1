bd7be77d0fb09c9d805722b91cacd1600501b309
nithesh <nitheshurao@gmail.com>
2020-09-22 10:37:21 +0530

    git deploy

---
+++ build/precache-manifest.f45065a2609ac67e763de9e951fb2516.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49791156ba3d4448535c504b62a16ba5",
+    "url": "/Profilss1/index.html"
+  },
+  {
+    "revision": "afd15b904bf568c9d0af",
+    "url": "/Profilss1/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/Profilss1/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/Profilss1/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afd15b904bf568c9d0af",
+    "url": "/Profilss1/static/js/main.f56b981e.chunk.js"
+  },
+  {
+    "revision": "485a1679491917983df6",
+    "url": "/Profilss1/static/js/runtime-main.6915925d.js"
+  }
+]);
